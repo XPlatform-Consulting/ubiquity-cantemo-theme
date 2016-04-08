4bc82942834bc2a6790b035e9289bebca8e0bbc4
Jared Smith <jared.smith.jrod@gmail.com>
2016-04-08 10:00:20 -0700

    XAOL-43 - Search Results - Media card menu options
    Added appropriate class names for options in the media item card menus
    and any other menu options that are added by plugins within cantemo.

--- portal_media/mdl/main.js
+++ portal_media/mdl/main.js
@@ -1,8 +1,28 @@
+'use strict';
+
+function addMdlToMenuItems(query) {
+    /**
+     *  Add the 'mdl-menu__item' class to all the elements found in the query
+     *  @param {string} query - CSS query
+     *  @return {Boolean} - Returns false if there's no items found in the query
+    **/
+    var allItems = document.querySelectorAll(query);
+
+    if (!allItems) {
+        return false;
+    }
+
+    for (var i = 0; i < allItems.length; i++) {
+        var item = allItems.item(i);
+
+        item.classList.add('mdl-menu__item');
+    }
+
+    return true;
+}
+
 window.addEventListener('DOMContentLoaded', function() {
-    /*
-    var mediaBin = document.querySelector('#mediabin-tab-show');
-    mediaBin.addEventListener('click', function() {
-        cntmo.app.page.mediaBin.toggle();
-    });
-    */
+    // this should get all the <a> elements in every mdl-menu
+    // on the page that isn't part of the .mdl-menu__item class
+    addMdlToMenuItems('.mdl-menu a:not(.mdl-menu__item)');
 });
