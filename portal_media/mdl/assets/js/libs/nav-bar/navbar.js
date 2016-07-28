84075972b339bf1751c8beb30b4abba51a5cc8af
garrett <garrett@highwaythreesolutions.com>
2016-07-28 13:22:51 -0700

    Cap auto commit

--- portal_media/mdl/assets/js/libs/nav-bar/navbar.js
+++ portal_media/mdl/assets/js/libs/nav-bar/navbar.js
@@ -9,9 +9,17 @@
         if($('button.cantemo-settings-button').is(e.target) || $('button.cantemo-settings-button').has(e.target).length > 0 ){
             $('.cantemo-settings-card-popup').toggleClass('active-popup');            
         }
-
+        
+        if($('button.cantemo-notifications-button').is(e.target) || $('button.cantemo-notifications-button').has(e.target).length > 0 ){
+            $('.cantemo-notifications-card-popup').toggleClass('active-popup');            
+        }
 
         // remove stuff
+
+        if (!$('.cantemo-notifications-button').is(e.target) && !$('.cantemo-notifications-card-popup').is(e.target) && $('.cantemo-notifications-card-popup').has(e.target).length === 0 && $('.cantemo-notifications-button').has(e.target).length === 0 ){ // if the target of the click isn't the container nor a descendant of the container
+            $('.cantemo-notifications-card-popup').removeClass('active-popup');
+        }
+
         if (!$('.cantemo-search-bar').is(e.target) && $('.cantemo-search-bar').has(e.target).length === 0 ){ // if the target of the click isn't the container nor a descendant of the container
             $('#main-row').removeClass('search-active');
             // remove search content too
