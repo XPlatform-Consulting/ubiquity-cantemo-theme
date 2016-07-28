ac38b907743fb043e70c0441c7a0ae4d6d9f1f69
garrett <garrett@highwaythreesolutions.com>
2016-07-28 13:01:26 -0700

    Cap auto commit

--- portal_media/mdl/assets/js/libs/nav-bar/navbar.js
+++ portal_media/mdl/assets/js/libs/nav-bar/navbar.js
@@ -1,30 +1,25 @@
 (function($) {
 
     $('html').on('click',function(e){
+        // add stuff
         if ($('.cantemo-search-bar').is(e.target) || $('.cantemo-search-bar').has(e.target).length > 0 ){ // if the target of the click isn't the container nor a descendant of the container
             $('#main-row').addClass('search-active');
         }
-    });
-    
-    $('#account').on('click', function(){
-        $('.cantemo-settings-card-popup').toggleClass('active-popup');
-    });
 
-    $('.cantemo-settings-button').on('click',function(){
-        $('.cantemo-settings-card-popup').toggleClass('active-popup');
-    });
+        if($('.cantemo-settings-button').is(e.target)){
+            $('.cantemo-settings-card-popup').toggleClass('active-popup');            
+        }
 
-	$('html').on('click',function(e){
+
+        // remove stuff
         if (!$('.cantemo-search-bar').is(e.target) && $('.cantemo-search-bar').has(e.target).length === 0 ){ // if the target of the click isn't the container nor a descendant of the container
             $('#main-row').removeClass('search-active');
             // remove search content too
         }
         // close accout settigns popup 
         if (!$('.cantemo-settings-button').is(e.target) && !$('.cantemo-settings-card-popup').is(e.target) && $('.cantemo-settings-card-popup').has(e.target).length === 0 && $('.cantemo-settings-button').has(e.target).length === 0 ){ // if the target of the click isn't the container nor a descendant of the container
-            // $('.cantemo-settings-card-popup').removeClass('active-popup');
+            $('.cantemo-settings-card-popup').removeClass('active-popup');
         }
     });
-
-    
     
 })(jQuery);
